7827c56486ea365f55e54f2afa9a7a854781277d
Claudia Valdivieso <claumavaldivieso@gmail.com>
2015-07-13 15:56:18 -0500

    commands to run in win and linux

--- selenium/run_hub.cmd
+++
@@ -1,3 +0,0 @@
-java -jar C:\selenium-grid-node\selenium-server-standalone.jar -role hub -hubConfig C:\selenium-grid-node\hubConfig.json
-
-rem java -jar /home/claudia/Downloads/selenium-server-standalone-2.46.0.jar -role hub -hubConfig selenium/hubConfig.json
--- selenium/start_local.cmd
+++
@@ -1,7 +0,0 @@
-java -Xrs -jar C:\selenium-grid-node\selenium-server-standalone.jar -role node -host %COMPUTERNAME% -nodeConfig C:\selenium-grid-node\localNodeConfig.json -Dwebdriver.chrome.driver="C:\selenium-grid-node\chromedriver.exe" -Dwebdriver.ie.driver="C:\selenium-grid-node\IEDriver.exe"
-
-rem java -jar C:\selenium-grid-node\selenium-server-standalone-2.46.0.jar -role node -nodeConfig C:\selenium-grid-node\localNodeConfig.json -Dwebdriver.chrome.driver="C:\Program Files\Google\Chrome\Application\chrome.exe" -Dwebdriver.ie.driver="C:\selenium-grid-node\IEDriverServer.exe" -hub http://10.54.12.104:4444/grid/register
-
-rem java -jar C:\selenium-grid-node\selenium-server-standalone-2.46.0.jar -role node -nodeConfig C:\selenium-grid-node\localNodeConfig.json -Dwebdriver.ie.driver="C:\selenium-grid-node\IEDriverServer.exe" -hub http://10.54.12.104:4444/grid/register
-
-rem java -jar /home/claudia/Downloads/selenium-server-standalone-2.46.0.jar -role node -nodeConfig /home/claudia/htdocs/galen-test-crossbrowser/selenium/localNodeConfig.json -Dwebdriver.chrome.driver="/home/claudia/Downloads/chromedriver"
